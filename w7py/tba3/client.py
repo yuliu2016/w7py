405f5baf9d05a192cf2f3d0379daa18d75981d36
yuliu2016 <yuliu2016@users.noreply.github.com>
2018-06-28 10:28:43 -0400

    integrate query_args

--- w7py/tba3/client.py
+++ w7py/tba3/client.py
@@ -8,17 +8,18 @@ import requests
 
 from const import *
 from .exceptions import *
+from .query_args import *
 
 
 class TBACachedSession:
     def __init__(self, parent_client: "TBAClient"):
         self.__parent_client = parent_client
-        self.__session_already_set = False
         self.is_connectible = True
         self.online_only = False
         self.session_cache = {}
         self.session_name = ""
         self.no_cache_value = "empty_dict"
+        self.query_args = None
 
     def __getattr__(self, item) -> "dict":
         if self.online_only:
@@ -32,16 +33,6 @@ class TBACachedSession:
         res = self.__parent_client.raw_json(item)
         self.session_cache[item] = res
         return res
-
-    def set_session(self, name: "str" = ""):
-        if type(name) is str and name.isalnum():
-            if not self.__session_already_set:
-                self.session_name = name
-                self.session_cache = self.__parent_client.load_cache_data(name)
-                self.__session_already_set = True
-            else:
-                raise TBASessionAlreadyUsedException("Session already set for {}"
-                                                     .format(self.session_name))
 
     def clear_cache(self):
         self.session_cache = {}
@@ -66,28 +57,14 @@ class TBAClient:
     def raw_json(self, url: "str") -> "dict":
         return requests.get(TBA_BASE_URL + url, headers=self.get_request_headers()).json()
 
-    def compute_paths(self, session_name):
-        if not os.path.exists(self.cache_directory):
-            os.makedirs(self.cache_directory)
-        json_path = os.path.join(self.cache_directory, "{}.json".format(session_name))
-        pkl_path = os.path.join(self.cache_directory, "{}.pkl".format(session_name))
-        pkl_exists = os.path.exists(pkl_path)
-        json_exists = os.path.exists(json_path)
-        return (json_path, json_exists), (pkl_path, pkl_exists)
-
-    def load_cache_data(self, session_name):
-        _, (pkl_path, pkl_exists) = self.compute_paths(session_name)
-        if pkl_exists:
-            with open(pkl_path, "rb") as pickle_cache:
-                return pickle.load(pickle_cache)
-        return {}
-
     @contextmanager
     def cached_session(self,
                        online_only: "bool" = False,
                        no_cache_value: "str" = "empty_dict",
-                       connection_test_ip='http://216.58.192.142',
-                       connection_test_query="/team/frc865"):
+                       existing_args=None,
+                       **preset_tba_args):
+        connection_test_ip: "str" = 'http://216.58.192.142'
+        connection_test_tba_query: "str" = "/team/frc865"
         is_connectible = True
         if self.auth_key:
             try:
@@ -97,7 +74,7 @@ class TBAClient:
                 if online_only:
                     raise requests.ConnectionError("Cannot Connect to the Internet")
             if is_connectible:
-                res = self.raw_json(connection_test_query)  # Test for the Blue Alliance connection
+                res = self.raw_json(connection_test_tba_query)  # Test for the Blue Alliance connection
                 if len(res.keys()) == 1:
                     is_connectible = False
                     if online_only:
@@ -106,25 +83,42 @@ class TBAClient:
             is_connectible = False
             if online_only:
                 raise ValueError("No TBA key set. Get one from the website!")
+        if type(existing_args) is TBAQueryArguments:
+            q_args = existing_args
+        else:
+            q_args = TBAQueryArguments(**preset_tba_args)
+        if q_args:
+            session_id = str(q_args)
+        else:
+            session_id = "main"
+        if not os.path.exists(self.cache_directory):
+            os.makedirs(self.cache_directory)
+        json_path = os.path.join(self.cache_directory, "{}.json".format(session_id))
+        pkl_path = os.path.join(self.cache_directory, "{}.pkl".format(session_id))
+        pkl_exists = os.path.exists(pkl_path)
+        json_exists = os.path.exists(json_path)
+        loaded_cache = {}
+        if pkl_exists:
+            with open(pkl_path, "rb") as pickle_cache:
+                loaded_cache = pickle.load(pickle_cache)
         session = TBACachedSession(self)
+        session.session_cache = loaded_cache
         session.is_connectible = is_connectible
         session.online_only = online_only
         session.no_cache_value = no_cache_value
+        session.query_args = q_args
         yield session
-        res_name = session.session_name
-        if res_name:
-            res_cache = session.session_cache
-            (json_path, json_exists), (pkl_path, pkl_exists) = self.compute_paths(res_name)
-            if not res_cache:  # File need to be removed here due to clear_cache
-                if json_exists:
-                    os.remove(json_path)
-                if pkl_exists:
-                    os.remove(pkl_path)
-            else:
-                with open(json_path, "w") as json_file:
-                    json.dump(res_cache, json_file, indent=4)
-                with open(pkl_path, "wb") as pickle_file:
-                    pickle.dump(res_cache, pickle_file)
+        res_cache = session.session_cache
+        if res_cache:
+            with open(json_path, "w") as json_file:
+                json.dump(res_cache, json_file, indent=4)
+            with open(pkl_path, "wb") as pickle_file:
+                pickle.dump(res_cache, pickle_file)
+        else:  # File need to be removed here due to clear_cache
+            if json_exists:
+                os.remove(json_path)
+            if pkl_exists:
+                os.remove(pkl_path)
 
 
 client_instance = TBAClient()
