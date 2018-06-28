452769428d627f3686f012fbd90b69207bcf7fea
yuliu2016 <yuliu2016@users.noreply.github.com>
2018-06-28 11:16:48 -0400

    merge query_args

--- w7py/tba3/client.py
+++ w7py/tba3/client.py
@@ -7,7 +7,6 @@ from urllib import request as urllib_request
 import requests
 
 from .exceptions import *
-from .query_args import *
 
 
 class TBAClient:
@@ -137,6 +136,26 @@ class TBACachedSession:
 
     def clear_cache(self):
         self.session_cache = {}
+
+
+class TBAQueryArguments:
+    ARGS_KEYS = ["team_key", "district_key", "match_key", "event_key", "year", "media_tag", "page_num"]
+
+    def __init__(self, **tba_args):
+        self.tba_args = tba_args
+
+    def create_url(self, query_template: str, **union_args):
+        new_args = {**self.tba_args, **union_args}
+        try:
+            return query_template.format(**new_args)
+        except KeyError as err:
+            raise TBARequiredArgumentNotSpecified("Argument " + str(err) + " was not specified")
+
+    def __str__(self):
+        return "-".join(self.tba_args[key] for key in sorted(self.tba_args.keys()) if key in self.ARGS_KEYS)
+
+    def __bool__(self):
+        return bool(list(key for key in self.tba_args.keys() if key in self.ARGS_KEYS))
 
 
 client_instance = TBAClient()
