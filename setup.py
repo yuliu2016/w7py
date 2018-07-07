6ad0b40e2de3816812c403935a7b918a72b332b0
yuliu2016 <yuliu2016@users.noreply.github.com>
2018-07-06 22:00:32 -0400

    update setup and requirements

--- setup.py
+++ setup.py
@@ -8,18 +8,21 @@ if __name__ == "__main__":
         name="w7py",
         version="0.0.1",
         description="w7py",
+        author="author",
+        author_email="author@example.com",
         long_description=long_description,
         url="https://github.com/yuliu2016/w7py",
         packages=setuptools.find_packages(),
-        classifiers=(
+        classifiers=[
             "Programming Language :: Python :: 3",
             "Operating System :: OS Independent",
-        ),
+        ],
         install_requires=[
             "numpy",
             "pandas",
             "requests"
         ],
+        python_requires=">=3.5",
         entry_points={
             'console_scripts': ['w7=w7py.cli:cli_main']
         }
